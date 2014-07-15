3988dcb2f8c78c40fd1a49521d0b3c3c0991da9b
Felipe Iketani <felipeiketani@gmail.com>
2014-07-15 13:48:20 -0300

    subscribe to mailchimp only on production

--- app/models/email_marketing_provider.rb
+++ app/models/email_marketing_provider.rb
@@ -1,10 +1,20 @@
 class EmailMarketingProvider
   def subscribe!(user)
+    if Rails.env.production?
+      perform_subscription!(user)
+    else
+      Rails.logger.info "[Marketing][SKIPPED] Subscribing #{user.email} to Mailchimp"
+    end
+  end
+
+  private
+
+  def perform_subscription!(user)
     mailchimp = Mailchimp::API.new(MAILCHIMP_API_KEY)
     subscription = mailchimp.lists.subscribe(MAILCHIMP_LIST_ID, { "email" => user.email })
-    Rails.logger.info "[Marketing] Subscribing #{user.email} on Mailchimp"
+    Rails.logger.info "[Marketing] Subscribing #{user.email} to Mailchimp"
   rescue => e
-    Rails.logger.info "[Marketing] Error subscribing #{user.email} on Mailchimp"
+    Rails.logger.info "[Marketing] Error subscribing #{user.email} to Mailchimp"
     Rails.logger.info "[Marketing] #{e.class} | #{e.message}"
   end
 end
